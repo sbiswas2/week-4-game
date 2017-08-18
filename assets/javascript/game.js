a76a8cd0ace5d69b4dd0c3e2d6e43bebf1cdb55e
Setu Biswas <setubiswas@Setus-MacBook-Pro.local>
2017-08-18 17:59:57 -0500

    outline of game

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// 4 buttons each given a random value at the beginning of the game (between 1-12)
+
+// Player is shown a random number at the beginning of the game (between 19-120)
+
+// Player guesses buttons - document.onclick
+
+// As player clicks, the amount is added to players score
+	// hide the number, only display the total score after it increases with each click
+
+// If player matches number then win, if scores goes above then lose
+
+// Game restarts after win or loss, keep track of wins and losses
+	// new random number, new random values for buttons
+	// user score counter reset back to zero
