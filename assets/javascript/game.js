f5490b4f6f1ff1758ec3d83f57f852c2c1238260
Setu Biswas <setubiswas@Setus-MacBook-Pro.local>
2017-08-19 17:33:54 -0500

    finished game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,55 +5,129 @@ var button1Value = Math.floor(Math.random() * ((buttonMax - buttonMin)+1) + butt
 var button2Value = Math.floor(Math.random() * ((buttonMax - buttonMin)+1) + buttonMin);
 var button3Value = Math.floor(Math.random() * ((buttonMax - buttonMin)+1) + buttonMin);
 var button4Value = Math.floor(Math.random() * ((buttonMax - buttonMin)+1) + buttonMin);
-console.log(button1Value);
-console.log(button2Value);
-console.log(button3Value);
-console.log(button4Value);
+	console.log(button1Value);
+	console.log(button2Value);
+	console.log(button3Value);
+	console.log(button4Value);
 
 // Player is shown a random number at the beginning of the game (between 19-120)
 var randomMin = 19;
-var randomMax = 120
+var randomMax = 120;
 var randomNumber = Math.floor(Math.random() * ((randomMax - randomMin)+1) + randomMin);
-console.log(randomNumber);
+$("#random-number").text(randomNumber);
+	console.log(randomNumber);
 
+// score set at 0, display score
 var score = 0;
-document.getElementById("score").innerHTML = score;
+$("#score").text(score);
 
-// create reset function here to call later
+// keep track of wins and losses
+var wins = 0;
+var losses = 0;
+$("#wins").text("Wins: " + wins);
+$("#losses").text("Losses: " + losses);
+
+// create reset function here to call later after a win or loss
 function newGame () {
-	
-}
-// Player guesses buttons - document.onclick
+	// new button values
+	button1Value = Math.floor(Math.random() * ((buttonMax - buttonMin)+1) + buttonMin);
+	button2Value = Math.floor(Math.random() * ((buttonMax - buttonMin)+1) + buttonMin);
+	button3Value = Math.floor(Math.random() * ((buttonMax - buttonMin)+1) + buttonMin);
+	button4Value = Math.floor(Math.random() * ((buttonMax - buttonMin)+1) + buttonMin);
+		console.log(button1Value);
+		console.log(button2Value);
+		console.log(button3Value);
+		console.log(button4Value);
+	// new random number value
+	randomNumber = Math.floor(Math.random() * ((randomMax - randomMin)+1) + randomMin);
+	$("#random-number").text(randomNumber);
+		console.log(randomNumber);
+	// user score back 0
+	score = 0;
+	$("#score").text(score);
+};
+
+// Player guesses button value when clicked
 	// As player clicks, the amount is added to players score
 	// hide the number, only display the total score after it increases with each click
-
 $("#btn-1").click(function() {
 	score = score + button1Value;
-	console.log(score);
-	document.getElementById("score").innerHTML = score;
+	$("#score").text(score);
+		console.log(score);
+	// If player matches number then win, if scores goes above then lose
+	if (score === randomNumber) {
+		wins++;
+		$("#result").text("You Win!");
+		$("#wins").text("Wins: " + wins);
+		$("#losses").text("Losses: " + losses);
+		newGame(); //reset variables
+	} else if (score > randomNumber) {
+		losses++;
+		$("#result").text("You Lose!");
+		$("#wins").text("Wins: " + wins);
+		$("#losses").text("Losses: " + losses);
+		newGame(); //reset variables
+	}
 });
 
 $("#btn-2").click(function() {
 	score = score + button2Value;
-	console.log(score);
-	document.getElementById("score").innerHTML = score;
+	$("#score").text(score);
+		console.log(score);
+	// If player matches number then win, if scores goes above then lose
+	if (score === randomNumber) {
+		wins++;
+		$("#result").text("You Win!");
+		$("#wins").text("Wins: " + wins);
+		$("#losses").text("Losses: " + losses);
+		newGame(); //reset variables
+	} else if (score > randomNumber) {
+		losses++;
+		$("#result").text("You Lose!");
+		$("#wins").text("Wins: " + wins);
+		$("#losses").text("Losses: " + losses);
+		newGame(); //reset variables
+	}
 });
 
 $("#btn-3").click(function() {
 	score = score + button3Value;
-	console.log(score);
-	document.getElementById("score").innerHTML = score;
+	$("#score").text(score);
+		console.log(score);
+	// If player matches number then win, if scores goes above then lose
+	if (score === randomNumber) {
+		wins++;
+		$("#result").text("You Win!");
+		$("#wins").text("Wins: " + wins);
+		$("#losses").text("Losses: " + losses);
+		newGame(); //reset variables
+	} else if (score > randomNumber) {
+		losses++;
+		$("#result").text("You Lose!");
+		$("#wins").text("Wins: " + wins);
+		$("#losses").text("Losses: " + losses);
+		newGame(); //reset variables
+	}
 });
 
 $("#btn-4").click(function() {
 	score = score + button4Value;
-	console.log(score);
-	document.getElementById("score").innerHTML = score;
+	$("#score").text(score);
+		console.log(score);
+	// If player matches number then win, if scores goes above then lose
+	if (score === randomNumber) {
+		wins++;
+		$("#result").text("You Win!");
+		$("#wins").text("Wins: " + wins);
+		$("#losses").text("Losses: " + losses);
+		newGame(); //reset variables
+	} else if (score > randomNumber) {
+		losses++;
+		$("#result").text("You Lose!");
+		$("#wins").text("Wins: " + wins);
+		$("#losses").text("Losses: " + losses);
+		newGame(); //reset variables
+	}
 });
-
-
-// If player matches number then win, if scores goes above then lose
-	// document write on a win or loss
-	// then reset values for buttons and random number
 
 
